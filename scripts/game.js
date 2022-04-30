63564266af6318ea480997db22cf2512a473d7d5
Milena <lenamsst@gmail.com>
2022-04-30 18:44:38 -0300

    feat: adicionando ações e simbolos

---
+++ scripts/game.js
@@ -0,0 +1,19 @@
+//iniciar as variáveis
+let board = ['', '', '', '', '', '', '', '', ''];
+let playTime = 0;
+let symbols = ['o','x'];
+
+
+function handleMove(position) {
+
+    if(board[position] == ''){
+        board[position] = symbols[playTime];
+
+        if(playTime == 0) {
+            playTime = 1;
+        } else {
+            playTime = 0;
+        }
+    }
+
+}
