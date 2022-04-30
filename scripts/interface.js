2c5d52bcf01cd649f06dcd0cb0321c8a1bc58498
Milena <lenamsst@gmail.com>
2022-04-30 19:23:23 -0300

    feat: verificando vencedor

--- scripts/interface.js
+++ scripts/interface.js
@@ -9,26 +9,23 @@ document.addEventListener('DOMContentLoaded',() =>{
 
 })
 
-function handleClick(event){
+function handleClick(event) {
 
     let square = event.target;
     let postion = square.id;
 
-    handleMove(postion);
-    updateSquares();
+    if (handleMove(postion)) {
+
+        setTimeout(() => {
+            alert ("Fim de Jogo! - O Vencedor foi " + playTime);
+        }, 10);      
+    };
+
+    updateSquare(postion);
 }
 
-function updateSquares() {
-
-    let squares = document.querySelectorAll(".square");
-
-    squares.forEach((square)=>{
-        let postion = square.id;
-        let symbol = board[postion];
-
-        if(symbol != ''){
-            square.innerHTML = `<div class='${symbol}'></div>`
-        }
-    })
-
+function updateSquare(postion) {
+    let square = document.getElementById(postion.toString());
+    let symbol = board[postion];
+    square.innerHTML = `<div class='${symbol}'></div>`
 }
